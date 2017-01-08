67461eca9838f99a3db982beaf0f520ea842318a
Jimmy Johnson <catch22@fastmail.net>
2017-01-07 20:59:55 -0800

    Reoving binaries

--- Debug/src/Libfreenect2OpenCV.d
+++
@@ -1,4 +0,0 @@
-src/Libfreenect2OpenCV.o: ../src/Libfreenect2OpenCV.cpp \
- ../src/Libfreenect2OpenCV.h
-
-../src/Libfreenect2OpenCV.h:
--- Debug/src/main.d
+++
@@ -1,3 +0,0 @@
-src/main.o: ../src/main.cpp ../src/Libfreenect2OpenCV.h
-
-../src/Libfreenect2OpenCV.h:
